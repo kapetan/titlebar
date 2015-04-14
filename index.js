a6a915852dcc41de41021169d7ef1e967e65081d
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2015-04-14 22:24:54 +0300

    Use dombo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var util = require('util');
 var fs = require('fs');
 var defaultcss = require('defaultcss');
 var domify = require('domify');
+var $ = require('dombo');
 
 var stoplight = require('./stoplight');
 
@@ -11,11 +12,6 @@ var html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
 
 style = util.format(style, stoplight);
 
-var $ = function(selector, context) {
-	context = context || document;
-	return context.querySelector(selector);
-};
-
 var TitleBar = function(options) {
 	if(!(this instanceof TitleBar)) return new TitleBar(options);
 
@@ -23,29 +19,30 @@ var TitleBar = function(options) {
 	this._options = options || {};
 
 	var element = domify(html);
+	var $element = $(element);
 	this.element = element;
 
 	var self = this;
-	var close = $('.titlebar-close', element);
-	var minimize = $('.titlebar-minimize', element);
-	var fullscreen = $('.titlebar-fullscreen', element);
+	var close = $('.titlebar-close', element)[0];
+	var minimize = $('.titlebar-minimize', element)[0];
+	var fullscreen = $('.titlebar-fullscreen', element)[0];
 
-	element.addEventListener('click', function(e) {
+	$element.on('click', function(e) {
 		if(e.target === close) self.emit('close', e);
 		if(e.target === minimize) self.emit('minimize', e);
 		if(e.target === fullscreen) self.emit('fullscreen', e);
-	}, false);
+	});
 
-	element.addEventListener('dblclick', function(e) {
+	$element.on('dblclick', function(e) {
 		if(e.target === close || e.target === minimize || e.target === fullscreen) return;
 		self.emit('maximize', e);
-	}, false);
+	});
 };
 
 util.inherits(TitleBar, events.EventEmitter);
 
 TitleBar.prototype.appendTo = function(element) {
-	if(typeof element === 'string') element = $(element);
+	if(typeof element === 'string') element = $(element)[0];
 	if(this._options.style !== false) defaultcss('titlebar', style);
 	element.appendChild(this.element);
 	return this;
